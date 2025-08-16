4125a0dc8b29e5832b2a43c21037198244e55647
Peter Cooney <petermcooney@gmail.com>
2025-08-16 19:35:33 +1000

    Add files via upload
    Initial commit of file from eth-ruby-tools project

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ruby:3.3
+
+RUN gem install eth rlp sinatra rackup puma rack-contrib
+WORKDIR /app
+
+COPY eth_tool.rb .
+COPY eth_tool_api.rb .
+
+ENV ETH_RPC_URL=http://geth:8545
+ENV KEYSTORE_DIR=/app/keystores
+
+RUN mkdir -p /app/keystores
+
+EXPOSE 3000
+ENTRYPOINT ["ruby", "/app/eth_tool_api.rb"]
---
+++ eth_tool.rb
@@ -0,0 +1,76 @@
+require 'eth'
+require 'fileutils'
+require 'json'
+require 'time'
+
+KEYSTORE_DIR = ENV['KEYSTORE_DIR'] || File.join(__dir__, 'keystores')
+FileUtils.mkdir_p(KEYSTORE_DIR) unless Dir.exist?(KEYSTORE_DIR)
+
+def get_utc_filename(address)
+  now = Time.now.utc
+  iso = now.iso8601.gsub(':', '-')
+  "UTC--#{iso}--#{address.downcase}"
+end
+
+def rpc_call(method, params = [])
+  uri = URI(ENV['ETH_RPC_URL'] || 'http://geth:8545')
+  body = { jsonrpc: '2.0', method: method, params: params, id: 1 }.to_json
+  response = Net::HTTP.post(uri, body, 'Content-Type' => 'application/json')
+  JSON.parse(response.body)['result']
+end
+
+def create_account(password)
+  raise ArgumentError, "Password cannot be empty" if password.nil? || password.empty?
+
+  key = Eth::Key.new
+  keystore = Eth::Key::Encrypter.perform(key, password)
+
+  filename = get_utc_filename(key.address.to_s)
+  path = File.join(KEYSTORE_DIR, filename)
+  File.write(path, keystore)
+  key.address.to_s
+end
+
+def send_transaction(address, password, to, amount_eth)
+  raise ArgumentError, "Invalid recipient address" unless to.match?(/^0x[a-fA-F0-9]{40}$/)
+  raise ArgumentError, "Invalid amount" unless amount_eth.to_s.match?(/^\d+(\.\d+)?$/) && amount_eth.to_f > 0
+
+  filename = Dir.children(KEYSTORE_DIR).find { |f| f.end_with?(address.downcase) }
+  raise "Keystore not found for address #{address}" unless filename
+
+  path = File.join(KEYSTORE_DIR, filename)
+  json = File.read(path)
+
+  begin
+    key = Eth::Key::Decrypter.perform(json, password)
+  rescue StandardError => e
+    raise "Failed to load keystore: #{e.message}"
+  end
+
+  nonce = rpc_call('eth_getTransactionCount', [key.address, 'pending']).to_i(16)
+  gas_price = rpc_call('eth_gasPrice').to_i(16)
+
+  tx = Eth::Tx.new(
+    nonce: nonce,
+    gas_price: gas_price,
+    gas_limit: 21_000,
+    to: to,
+    value: Eth::Utils.to_wei(amount_eth.to_f)
+  )
+
+  begin
+    tx.sign(key)
+  rescue StandardError => e
+    raise "Failed to sign transaction: #{e.message}"
+  end
+
+  raw_tx = '0x' + RLP.encode(tx).unpack1('H*')
+  tx_hash = rpc_call('eth_sendRawTransaction', [raw_tx])
+
+  unless tx_hash.is_a?(String) && tx_hash.match?(/^0x([A-Fa-f0-9]{64})$/)
+    raise "Invalid transaction hash format: #{tx_hash.inspect}"
+  end
+  puts tx_hash
+
+  tx_hash
+end
